2d0aa6bd1085bdd6310211efb939122d0a15fdab
thomasdubost <thomas.dubost@etu.u-paris.fr>
2025-10-29 23:15:58 +0000

    Refactor student management functions and logic

--- etudiant.c
+++ etudiant.c
@@ -6,7 +6,7 @@
 int main()
 {
     int etudiant_count = 0;
-    Etudiant etudiant_list[MAX_ETUDIANTS] = {0};
+    Etudiant etudiant_list[MAX_ETUDIANTS];
     char command[MAX_COMMAND_LENGTH];
     while (1)
     {
@@ -41,8 +41,11 @@ void choixCommande(char *command, int *etudiant_count, Etudiant *etudiant_list)
     case NOTE:
         note(separer_txt, etudiant_list, *etudiant_count);
         break;
-    case DEMISSION_DEFAILLANCE:
-        // demissionDefaillance();
+    case DEMISSION:
+        demission(separer_txt, etudiant_list, *etudiant_count);
+        break;
+    case DEFAILLANCE:
+        defaillance(separer_txt, etudiant_list, *etudiant_count);
         break;
     case JURY:
         jury(separer_txt, etudiant_list, *etudiant_count);
@@ -85,11 +88,11 @@ void separertxt(char *command, texte_separer *texte_separer)
     }
     else if (strcmp(token, "DEMISSION") == 0)
     {
-        texte_separer->command_type = DEMISSION_DEFAILLANCE;
+        texte_separer->command_type = DEMISSION;
     }
     else if (strcmp(token, "DEFAILLANCE") == 0)
     {
-        texte_separer->command_type = DEMISSION_DEFAILLANCE;
+        texte_separer->command_type = DEFAILLANCE;
     }
     else if (strcmp(token, "JURY") == 0)
     {
@@ -159,25 +162,24 @@ void cursus(const texte_separer separer_txt, int nb_etudiant, Etudiant *etudiant
 {
     if (separer_txt.arguments_cmp != 1)
     {
-        printf("Erreur : ID étudiant manquant.\n");
         return;
     }
 
     int id = atoi(separer_txt.list_arguments[0]);
-    Etudiant *etudiant = NULL;
-    for (int i = 0; i < nb_etudiant; i++)
+
+    if (id > nb_etudiant || id < 1)
     {
-        if (etudiant_list[i].id_etudiant == id)
-        {
-            etudiant = &etudiant_list[i];
-            break;
-        }
+        puts("Identifiant incorrect");
+        return;
     }
+
+    Etudiant *etudiant = &etudiant_list[id - 1];
+
     printf("%d %s %s\n", etudiant->id_etudiant, etudiant->prenom, etudiant->nom);
 
     affichage_semestre(*etudiant, etudiant->semestre_en_cours);
     // Statut final
-    switch (etudiant_list->status)
+    switch (etudiant->status)
     {
     case EN_COURS:
         printf("en cours\n");
@@ -185,7 +187,7 @@ void cursus(const texte_separer separer_txt, int nb_etudiant, Etudiant *etudiant
     case AJOURNE:
         printf("ajourne\n");
         break;
-    case DEMISSION:
+    case STATUT_DEMISSION:
         printf("demission\n");
         break;
     case DIPLOME:
@@ -211,10 +213,10 @@ void affichage_semestre(Etudiant etudiant, int nb_semestres)
                 printf("%.1f (%s)", note, app);
             else
                 printf("* (*)");
-            if (m < NB_UE - 1)
-                printf(" - ");
+            printf(" - ");
         }
-        printf(" - ");
+        if (s + 1 != nb_semestres)
+            printf("\n");
     }
 }
 void afficher_bilan(Etudiant etudiant, int)
@@ -230,59 +232,127 @@ void afficher_bilan(Etudiant etudiant, int)
                 printf("%.1f (%s)", note, app);
             else
                 printf("* (*)");
-            if (m < NB_UE - 1)
-                printf(" - ");
+            printf(" - ");
         }
-        printf(" - ");
     }
 }
 void note(const texte_separer separer_txt, Etudiant *etudiant_list, int nb_etudiant)
 {
     int id = atoi(separer_txt.list_arguments[0]);
+
+    if (id > nb_etudiant || id < 1)
+    {
+        puts("Identifiant incorrect");
+        return;
+    }
+
     int UE_index = atoi(separer_txt.list_arguments[1]) - 1;
+
+    if (UE_index < 0 || UE_index > NB_UE)
+    {
+        return;
+    }
+
     float note = atof(separer_txt.list_arguments[2]);
 
-    for (int i = 0; i < nb_etudiant; i++)
+    if (note < 0 || note > NOTE_MAX)
     {
-        if (etudiant_list[i].id_etudiant == id)
-        {
-            etudiant_list[i].semestres[0].UE[UE_index].note = note;
-
-            // Déterminer l'appréciation en fonction de la note
-            if (note >= 10)
-                strcpy(etudiant_list[i].semestres[0].UE[UE_index].appreciation, "ADM");
-            else if (note <= 10 && note >= 8)
-                strcpy(etudiant_list[i].semestres[0].UE[UE_index].appreciation, "AJ");
-            else if (note < 8)
-                strcpy(etudiant_list[i].semestres[0].UE[UE_index].appreciation, "AJB");
-            else if (note < 8 && (etudiant_list[i].semestres[0].UE[UE_index].note + etudiant_list[i].semestres[1].UE[UE_index].note) / 2)
-
-                strcpy(etudiant_list[i].semestres[0].UE[UE_index].appreciation, "ADC");
-            else
-                strcpy(etudiant_list[i].semestres[0].UE[UE_index].appreciation, "ADS");
-
-            printf("Note enregistree \n");
-            return;
-        }
+        return;
     }
+
+    Etudiant *etudiant = &etudiant_list[id - 1];
+
+    etudiant->semestres[0].UE[UE_index].note = note;
+
+    // Déterminer l'appréciation en fonction de la note
+    if (note >= 10)
+        strcpy(etudiant->semestres[0].UE[UE_index].appreciation, "ADM");
+    else if (note <= 10 && note >= 8)
+        strcpy(etudiant->semestres[0].UE[UE_index].appreciation, "AJ");
+    else if (note < 8)
+        strcpy(etudiant->semestres[0].UE[UE_index].appreciation, "AJB");
+    else if (note < 8 && (etudiant->semestres[0].UE[UE_index].note + etudiant->semestres[1].UE[UE_index].note) / 2)
+
+        strcpy(etudiant->semestres[0].UE[UE_index].appreciation, "ADC");
+    else
+        strcpy(etudiant->semestres[0].UE[UE_index].appreciation, "ADS");
+
+    printf("Note enregistree \n");
+    return;
 }
+
 void jury(const texte_separer separer_txt, Etudiant *etudiant_list, int nb_etudiant)
 {
     int semestre = atoi(separer_txt.list_arguments[0]);
+    if (semestre < 1 || semestre > NB_SEMESTRES)
+    {
+        puts("Semestre incorrect");
+    }
+
     for (int i = 0; i < nb_etudiant; ++i)
     {
         Etudiant *etudiant = &etudiant_list[i];
-        if (semestre != etudiant->semestre_en_cours)
+        if (semestre != etudiant->semestre_en_cours || etudiant->status != EN_COURS)
         {
             continue;
         }
-        int ue_toutes_notee = 0;
         for (int j = 0; j < NB_UE; ++j)
         {
+            if (etudiant->semestres[semestre].UE[j].note == -1)
+            {
+                puts("Des notes sont manquantes");
+                return;
+            }
         }
     }
+    int nb_etudiant_affectes = 0;
+    for (int i = 0; i < nb_etudiant; ++i)
+    {
+        Etudiant *etudiant = &etudiant_list[i];
+        if (semestre - 1 != etudiant->semestre_en_cours || etudiant->status != EN_COURS)
+        {
+            continue;
+        }
+        etudiant->semestre_en_cours++;
+        nb_etudiant_affectes++;
+    }
+    printf("Semestre termine pour %d etudiant(s)\n", nb_etudiant_affectes);
 }
-void demissionDefaillance(const texte_separer separertxt, Etudiant *etudiant_list)
+void demission(const texte_separer separer_txt, Etudiant *etudiant_list, int nb_etudiant)
 {
-    int id = atoi(separertxt.list_arguments[0]);
+    int id = atoi(separer_txt.list_arguments[0]);
+    if (id <= 0 || id >= nb_etudiant)
+    {
+        puts("Identifiant incorrect");
+        return;
+    }
+
+    Etudiant *etudiant = &etudiant_list[id - 1];
+
+    if (etudiant->status != EN_COURS)
+    {
+        puts("Etudiant hors formation");
+        return;
+    }
+    etudiant->status = STATUT_DEMISSION;
+    puts("Demission enregistree");
+}
+void defaillance(const texte_separer separer_txt, Etudiant *etudiant_list, int nb_etudiant)
+{
+    int id = atoi(separer_txt.list_arguments[0]);
+    if (id <= 0 || id >= nb_etudiant)
+    {
+        puts("Identifiant incorrect");
+        return;
+    }
+
+    Etudiant *etudiant = &etudiant_list[id - 1];
+
+    if (etudiant->status != EN_COURS)
+    {
+        puts("Etudiant hors formation");
+        return;
+    }
+    etudiant->status = DEFAILLANT;
+    puts("Defaillance enregistree");
 }
